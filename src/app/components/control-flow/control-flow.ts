b368725d28bf5b154b509b4036b13e689eae7ffc
Sumit <sumitbasu116@gmail.com>
2025-08-01 00:30:13 +0530

    Control Flow Statement part5-if-else ladder

--- src/app/components/control-flow/control-flow.ts
+++ src/app/components/control-flow/control-flow.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css'
 })
 export class ControlFlow {
 
   isVisiblePara: boolean = false;
+
+  name: string = 'feb';
 
   showP() {
     this.isVisiblePara = true
