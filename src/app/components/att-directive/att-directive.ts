3c56babcb8eea813f203baaa6d7ebf28663003e8
Sumit <sumitbasu116@gmail.com>
2025-08-03 21:45:36 +0530

    Directives part4-basic coding before introducing ngStyle directive

--- src/app/components/att-directive/att-directive.ts
+++ src/app/components/att-directive/att-directive.ts
@@ -11,7 +11,13 @@ export class AttDirective {
 
   div1ClassName = signal<string>("");
 
+  isDiv2Green = signal<boolean>(false);
+
   setBgClass(className:string){
     this.div1ClassName.set(className);
+  }
+
+  toggleBgColor(){
+    this.isDiv2Green.set(!this.isDiv2Green());
   }
 }
