938ca14f1a693f8ca81dff7964c483ba69b3b7fd
Sumit <sumitbasu116@gmail.com>
2025-08-03 20:25:58 +0530

    Routing part2-include basic Bootstrap Nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+         path:'admin',
+         component:Admin
+    },
+    {
+         path:'control-flow-statement',
+         component:ControlFlow
+    },
+    {
+         path:'data-binding',
+         component:DataBinding
+    },
+    {
+         path:'signal',
+         component:DataBinding
+    }
+];
