8814247dc028b0ebe31ec466fb5148b0806d2001
Sumit <sumitbasu116@gmail.com>
2025-08-08 00:35:50 +0530

    Post API part1-user CRUD basic coding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { DataBinding } from './components/data-binding/data-binding';
 import { SignalEx } from './components/signal-ex/signal-ex';
 import { AttDirective } from './components/att-directive/att-directive';
 import { Getapi } from './components/getapi/getapi';
+import { User } from './components/user/user';
 
 export const routes: Routes = [
     {
@@ -30,5 +31,9 @@ export const routes: Routes = [
     {
         path:'api-get',
         component:Getapi
+    },
+    {
+        path:'user',
+        component:User
     }
 ];
