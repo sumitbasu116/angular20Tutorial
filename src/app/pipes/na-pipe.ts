f9e215461d3d4c3c580343eac2cc7bc826189356
Sumit <sumitbasu116@gmail.com>
2025-08-11 09:59:34 +0530

    Custom Pipe part1-ng g pipe

---
+++ src/app/pipes/na-pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'na'
+})
+export class NaPipe implements PipeTransform {
+
+  transform(value: any): unknown {
+    if(value!=null && value!==undefined && value!==''){
+      return value;
+    } else{
+      return 'NA';
+    }
+  }
+}
