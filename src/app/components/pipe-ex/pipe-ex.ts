83c4c57df58bd7d0e153cdb86b036f5760893548
Sumit <sumitbasu116@gmail.com>
2025-08-11 09:52:38 +0530

    Pipe and Custom Pipe part3-json pipe to show object

--- src/app/components/pipe-ex/pipe-ex.ts
+++ src/app/components/pipe-ex/pipe-ex.ts
@@ -1,10 +1,10 @@
-import { DatePipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, JsonPipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe-ex',
   imports: [LowerCasePipe,UpperCasePipe,TitleCasePipe,DatePipe,
-    SlicePipe
+    SlicePipe,JsonPipe
   ],
   templateUrl: './pipe-ex.html',
   styleUrl: './pipe-ex.css'
@@ -19,4 +19,12 @@ export class PipeEx {
   currDate: Date = new Date();
 
   rollNumber: number[] = [11,12,13,14,15,156];
+  studentObj:any = {
+    name:'Sumit',
+    mobile:'872383278',
+    address:{
+      city: 'Kolkata',
+      state: 'WB'
+    }
+  }
 }
