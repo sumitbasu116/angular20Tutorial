bb3a08c138d56fcaef4c11ef1156999e3e6db7a0
Sumit <sumitbasu116@gmail.com>
2025-08-13 10:19:32 +0530

    Service in Angular part2-constructor injection of HttpClient service

--- src/app/services/master.ts
+++ src/app/services/master.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,11 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class Master {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getSum(num1: number,num2: number){
     debugger;
     const res = num1 + num2;
     return res;
+  }
+
+  getUsers(){
+    return this.http.get('https://api.freeprojectapi.com/api/GoalTracker/getAllUsers');
   }
 }
