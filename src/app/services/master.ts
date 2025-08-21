c5744ffcfa9f9f62fb4f3154394d3e7ad7813f89
Sumit <sumitbasu116@gmail.com>
2025-08-21 12:09:21 +0530

    Hard Code Login part8-create subject to display loggedin user on login without reload part1

--- src/app/services/master.ts
+++ src/app/services/master.ts
@@ -1,11 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Master {
   appName : string = "Angular App";
+
+  onLogin: Subject<boolean> = new Subject<boolean>();
+
   constructor(private http: HttpClient) { }
   
   getSum(num1: number,num2: number){
