03f6f79985230a8c6e0a0d5889e64bb5e90c94b4
Sumit <sumitbasu116@gmail.com>
2025-08-21 10:28:03 +0530

    Hard Code Login part3-login page basic design code part3

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  loginObj:any={
+    username: '',
+    password:''
+  }
+
+  onLogin(){
+    
+  }
 }
